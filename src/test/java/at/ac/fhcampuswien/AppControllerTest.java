9c2f7d86a0d1c3ad02fa0dd732700695b121c0eb
gustavopdzwtFHCAMP <gustavo.podzuweit@stud.fh-campuswien.ac.at>
2022-03-03 11:15:38 +0100

    testing testing tests

--- src/test/java/at/ac/fhcampuswien/AppControllerTest.java
+++ src/test/java/at/ac/fhcampuswien/AppControllerTest.java
@@ -3,17 +3,25 @@ package at.ac.fhcampuswien;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AppControllerTest {
 
     @Test
+    public void setArticlesTest() {
+        List<Article> articles = new ArrayList<Article>();
+        AppController appController = new AppController();
+        appController.setArticles(articles);
+        assertEquals(articles.size(), appController.getArticleCount());
+    }
+
+    @Test
     public void getArticleCountTest(){
         AppController appController = new AppController();
+        appController.setArticles(new ArrayList<Article>());
         assertEquals(0,appController.getArticleCount());
-
     }
 
 
