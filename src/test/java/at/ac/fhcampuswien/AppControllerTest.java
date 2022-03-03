1d120a2bba5f1d22c3cc16aa026091e1b1a9a815
chrop01 <91630967+chrop01@users.noreply.github.com>
2022-03-03 19:46:31 +0100

    Added @BeforeAll to just instance once the AppController

--- src/test/java/at/ac/fhcampuswien/AppControllerTest.java
+++ src/test/java/at/ac/fhcampuswien/AppControllerTest.java
@@ -1,6 +1,7 @@
 package at.ac.fhcampuswien;
 
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.*;
@@ -9,73 +10,52 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class AppControllerTest {
 
+    private static AppController appController;
+
+    @BeforeAll
+    static void setup() {
+        appController = new AppController();
+    }
+
+
     @Test
     public void setArticlesTest() {
         List<Article> articles = new ArrayList<Article>();
-        AppController appController = new AppController();
         appController.setArticles(articles);
         assertEquals(articles.size(), appController.getArticleCount());
     }
 
     @Test
     public void getArticleCountTest() {
-        AppController appController = new AppController();
         appController.setArticles(new ArrayList<Article>());
         assertEquals(0, appController.getArticleCount());
     }
 
     @Test
-    public void getTopHeadlinesAustriaTest1(){
-        AppController appController = new AppController();
+    public void getTopHeadlinesAustriaTest1() {
         assertEquals(new ArrayList<Article>(), appController.getTopHeadlinesAustria());
     }
 
     @Test
-    public void getTopHeadlinesAustriaTest2(){
+    public void getTopHeadlinesAustriaTest2() {
         List<Article> headlines = new ArrayList<Article>();
-        AppController appController = new AppController();
         appController.setArticles(headlines);
         assertEquals(headlines, appController.getTopHeadlinesAustria());
     }
-
-   /* @Test
-        public void getFilterList(){
-        AppController appController = new AppController();
-        List<Article> articles = new ArrayList<Article>();
-        articles.add(new Article("author1","bitcoin"));
-        articles.add(new Article("author2","football"));
-        articles.add(new Article("author3","politics"));
-        articles.add(new Article("author4","doge"));
-        articles.add(new Article("author5","sports"));
-        //appController.filterList(articles, "bitcoin");
-
-        List<Article> testlist = new ArrayList<Article>();
-        testlist.add(new Article("author1","bitcoin"));
-        assertEquals(testlist, appController.filterList(articles, "bitcoin"));
-    }*/
 
     /***
      * Test of the filter list size == 0 (implement -> get the articles list from actual articles not manually created)
      */
     @Test
     public void filterListTest() {
-        AppController appController = new AppController();
         List<Article> articles = new ArrayList<Article>();
-        articles.add(new Article("author1","bitcoin"));
-        articles.add(new Article("author2","football"));
-        articles.add(new Article("author3","politics"));
-        articles.add(new Article("author4","doge"));
-        articles.add(new Article("author5","sports"));
+        articles.add(new Article("author1", "bitcoin"));
+        articles.add(new Article("author2", "football"));
+        articles.add(new Article("author3", "politics"));
+        articles.add(new Article("author4", "doge"));
+        articles.add(new Article("author5", "sports"));
 
         //assertEquals(appController.filterList(appController.getArticles(),"").size(), 0);
-        assertEquals(appController.filterList(articles,"").size(), 5);
+        assertEquals(appController.filterList(articles, "").size(), 5);
     }
-
-
-
-
-
-
-
-
 }
