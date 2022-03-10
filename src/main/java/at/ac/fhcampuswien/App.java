875177bb09e8c3f71c34a180255653c75dc29eb6
Timmy <timmy.nicholson@hotmail.com>
2022-03-10 14:26:13 +0100

    added JavaFX dependencies

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -1,9 +1,32 @@
 package at.ac.fhcampuswien;
 
-public class App {
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class App extends Application {
     public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.start();
+        launch(args);
 
 
 
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Hello World!");
+        Button btn = new Button();
+        btn.setText("Hello JavaFX!");
+        btn.setOnAction( (event) -> Platform.exit() );
+        Pane root = new StackPane();
+        root.getChildren().add(btn);
+        stage.setScene(new Scene(root, 300, 150));
+        stage.show();
     }
 }
