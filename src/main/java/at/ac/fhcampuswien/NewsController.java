e2732c9d837262ce4d6f63718c1f20a158df5d16
chrop01 <91630967+chrop01@users.noreply.github.com>
2022-03-11 22:13:09 +0100

    Added tableview and tabpane

--- src/main/java/at/ac/fhcampuswien/NewsController.java
+++ src/main/java/at/ac/fhcampuswien/NewsController.java
@@ -4,7 +4,10 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -16,23 +19,16 @@ public class NewsController {
     @FXML
     private Button exitButton;
 
+    @FXML
+    private Button btnGetToplinesAustria;
+
+    @FXML
+    private TableView<?> tvNews;
+
 
 
     //needed: 4 BUTTONS
     // ELEMENT for "get top headlines austria || ELEMENT for "get all news btc" || LABEL or smth for display Count Articles || QUIT Program - maybe also button and Alert -> "quitting text"
-
-    @FXML
-    private Label label2;
-
-    public void initialize() {
-        // TODO
-    }
-
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        label2.setText("Hello World!");
-    }
-
 
     @FXML
     public void exitApplication(){
@@ -45,6 +41,33 @@ public class NewsController {
                 });
             }
         }, 1000l);
+    }
+
+    /***
+     * test list for testing
+     * @param event
+     */
+    @FXML
+    void GetTopLinesAustria(ActionEvent event) {
+        List<Article> testlist = new ArrayList<Article>();
+        Article emp1 = new Article("Liam","20000");
+        testlist.add(emp1);
+        Article emp2 = new Article("Noah","2002300");
+        testlist.add(emp2);
+        Article emp3 = new Article("William","2011000");
+        testlist.add(emp3);
+        Article emp4 = new Article("James","31000");
+        testlist.add(emp4);
+        Article emp5 = new Article("Benjamin","2X0");
+        testlist.add(emp5);
+
+
+        /*tbv.getColumns().add(cl1);
+        tbv.getColumns().add(cl2);
+        for (Employee emp : employeeList){
+            tbv.getItems().add(emp);
+        }*/
+
     }
 
 }
