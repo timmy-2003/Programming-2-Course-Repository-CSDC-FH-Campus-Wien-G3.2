abc54ecb3dc7e3fc94b91e869049eb60c612d617
Timmy <timmy.nicholson@hotmail.com>
2022-03-30 16:09:18 +0200

    added NewsResponse class

---
+++ src/main/java/at/ac/fhcampuswien/NewsResponse.java
@@ -0,0 +1,22 @@
+package at.ac.fhcampuswien;
+
+import java.util.List;
+
+public class NewsResponse {
+
+    private String status;
+    private int totalResults;
+    private List<Article> articles;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public int getTotalResults() {
+        return totalResults;
+    }
+
+    public List<Article> getArticles() {
+        return articles;
+    }
+}
