8b03f54903befbc96bc97e8d79ea2337eecb9819
Timmy <timmy.nicholson@hotmail.com>
2022-04-03 12:49:53 +0200

    added new attributes (+ get methods) to the Article class

--- src/main/java/at/ac/fhcampuswien/Article.java
+++ src/main/java/at/ac/fhcampuswien/Article.java
@@ -4,6 +4,21 @@ public class Article {
 
     private String author;
     private String title;
+    private String url;
+    private String urlToImage;
+    private String publishedAt;
+    private String content;
+    private String description;
+
+    public Article(String author, String title, String url, String urlToImage, String publishedAt, String content, String description) {
+        this.author = author;
+        this.title = title;
+        this.url = url;
+        this.urlToImage = urlToImage;
+        this.publishedAt = publishedAt;
+        this.content = content;
+        this.description = description;
+    }
 
     public Article(String author, String title) {
         this.author = author;
@@ -25,5 +40,25 @@ public class Article {
     @Override
     public String toString() {
         return "Title: " + title + "\t" + "Author: " + author;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getUrlToImage() {
+        return urlToImage;
+    }
+
+    public String getPublishedAt() {
+        return publishedAt;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getDescription() {
+        return description;
     }
 }
