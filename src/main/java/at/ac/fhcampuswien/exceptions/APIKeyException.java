4fc4e961614d263ae2d7f823d65c89beb29a6515
Konstant-Coding <konstantin.papirnik@gmail.com>
2022-05-29 18:11:07 +0200

    Set Error Message forNewsAPIKeyException.java

--- src/main/java/at/ac/fhcampuswien/exceptions/APIKeyException.java
+++ src/main/java/at/ac/fhcampuswien/exceptions/APIKeyException.java
@@ -1,6 +1,16 @@
 package at.ac.fhcampuswien.exceptions;
 
+import at.ac.fhcampuswien.gui.NewsController;
+import at.ac.fhcampuswien.gui.NewsControllerAlertScreen;
+
 public class APIKeyException extends Exception {
 
+    private final static String ErrorMessage = "Your API Key reached it's limit, please check API Key!";
 
+    public APIKeyException() {
+        super(ErrorMessage);
+
+
+
+    }
 }
