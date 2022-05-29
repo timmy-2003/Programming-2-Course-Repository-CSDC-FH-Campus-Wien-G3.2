9db7c698efc4accee87640adf6c161ad21f07904
Konstant-Coding <konstantin.papirnik@gmail.com>
2022-05-30 01:05:18 +0200

    Updated HttpException

--- src/main/java/at/ac/fhcampuswien/exceptions/HttpException.java
+++ src/main/java/at/ac/fhcampuswien/exceptions/HttpException.java
@@ -1,4 +1,19 @@
 package at.ac.fhcampuswien.exceptions;
 
-public class HttpException {
+public class HttpException extends Exception {
+
+    private final static String ErrorMessage = "Status of API Response is not 'ok'!";
+    public HttpException() {
+        super(ErrorMessage);
+    }
+
+    HttpException(String message) {
+        super(ErrorMessage);
+    }
+
+    public static boolean checkStatus(String response) throws HttpException {
+        //check if status is "ok"
+        String rString = response.replaceAll("[^a-zA-Z]+", "").toLowerCase();
+        return rString.contains("statusok");
+    }
 }
