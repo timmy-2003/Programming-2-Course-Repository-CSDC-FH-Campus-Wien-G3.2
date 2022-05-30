f745f292d8928b63d7a4107a36e8b30c4af87e69
Konstant-Coding <konstantin.papirnik@gmail.com>
2022-05-30 16:19:02 +0200

    NewsAPIException.java added because branch and merge fun

--- src/main/java/at/ac/fhcampuswien/exceptions/NewsAPIException.java
+++ src/main/java/at/ac/fhcampuswien/exceptions/NewsAPIException.java
@@ -1,4 +1,14 @@
 package at.ac.fhcampuswien.exceptions;
 
 public class NewsAPIException extends Exception {
+
+    private final static String ErrorMessage = "A problem happened while getting your specified articles";
+
+    NewsAPIException() {
+        super(ErrorMessage);
+    }
+
+    NewsAPIException(String ErMessage) {
+        super(ErMessage);
+    }
 }
